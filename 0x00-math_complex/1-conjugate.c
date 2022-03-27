3030a732e2abba0160be4eb96490ec5d2640cba4
21Kgold <claudiarpa@gmail.com>
2022-03-26 18:48:14 -0700

    update

---
+++ 0x00-math_complex/1-conjugate.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * conjugate - returns the conjugate of a given complex number, in other words
+ * it changes the sign of the imaginary part of the complex.
+ * @c: data with type struct complex
+ * Return: complex type
+ */
+complex conjugate(complex c)
+{
+	double a = c.re;
+	double b = -1 * (c.im);
+}
